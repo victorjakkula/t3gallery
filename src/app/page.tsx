95ff7e1b5490bd770ce5265818900cc7f380484f
giri4800 <girishbadiger996@gmail.com>
2025-02-24 20:12:43 +0530

    ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
 import Link from "next/link";
+const mockUrl = ["https://w6l4i72ztb.ufs.sh/f/tK37us19w7TVqTTr9HStGkQF5X1f3IWceTErA2OJudHY7jwg",
+"https://w6l4i72ztb.ufs.sh/f/tK37us19w7TV3CjvzYlPcjWltvwBb2RTFrGn8p3NYZd0J5XM",
+"https://w6l4i72ztb.ufs.sh/f/tK37us19w7TVSB18IfmsHTFheVxOwZjBgQPS6Gr8vdYo57MX",
+"https://w6l4i72ztb.ufs.sh/f/tK37us19w7TV7UA5s0C3sbfLTZlqHNvnu4RYE1ptgMcxO09o",
+"https://w6l4i72ztb.ufs.sh/f/tK37us19w7TVvPhJyxgbj1mCORYN2pfPdk3AMicXgVoKqsWz"
+ ]
+  const mockImage = mockUrl.map((url, index) => ({
+    id: index+1,
+    url,
 
+  }))
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-       hello there how are you
+      <div className="flex flex-wrap gap-4">{
+     [...mockImage,...mockImage,...mockImage].map((image, index) => (
+        <div key={image.id} className="w-48">
+          <img src={image.url} />
+        </div>
+      ))}
+       
+
       </div>
     </main>
   );
